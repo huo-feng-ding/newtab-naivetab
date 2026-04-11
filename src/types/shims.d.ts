acf4493d28d7358a04db29ec87466f98eca1bd1b
GXFG <gaofengim@live.com>
2026-04-11 21:38:22 +0800

    feat: move webext-bridge protocol definitions to shims.d.ts and remove shim.d.ts

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
+import type { ProtocolWithReturn } from 'webext-bridge'
 import { DefineComponent } from 'vue'
 
 declare module '*.vue' {
@@ -9,4 +10,13 @@ declare module '*.vue' {
 declare module '*.md' {
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module 'webext-bridge' {
+  export interface ProtocolMap {
+    // define message protocol types
+    // see https://github.com/antfu/webext-bridge#type-safe-protocols
+    'tab-prev': { title: string | undefined }
+    'get-current-tab': ProtocolWithReturn<{ tabId: number }, { title?: string }>
+  }
 }
